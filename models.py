0ddcda993828b599ef05f28e71635c507dd2c496
dariayakovchuk <110456977+dariayakovchuk@users.noreply.github.com>
2025-06-05 10:17:49 +0200

    Update models.py

--- models.py
+++ models.py
@@ -43,7 +43,7 @@ class GCN(nn.Module):
             )
         )
         self.bns = nn.ModuleList()
-        self.bns.append(nn.BatchNorm1d(self.hidden_channels))
+        self.bns.append(pyg_nn.LayerNorm(self.hidden_channels))
 
         for _ in range(num_layers - 1):
             self.convs.append(
@@ -52,7 +52,12 @@ class GCN(nn.Module):
                     self.hidden_channels,
                 )
             )
-            self.bns.append(nn.BatchNorm1d(self.hidden_channels))
+            self.bns.append(pyg_nn.LayerNorm(self.hidden_channels))
+          
+        self.conv_last = pyg_nn.GCNConv(
+            self.hidden_channels,
+            self.hidden_channels,
+        )
 
         self.dropout = dropout
         self.activation = f.relu
@@ -76,17 +81,17 @@ class GCN(nn.Module):
             The output of the model.
         """
         x, ei, ew, batch = data.x, data.edge_index, data.edge_attr, data.batch
-        x = f.dropout(x, p=self.dropout, training=self.training)
         for i, conv in enumerate(self.convs):
             if self.weighted:
                 x = conv(x, ei, edge_weight=ew)
             else: 
                 x = conv(x, ei)
             if self.use_bn:
-                x = self.bns[i](x)
+                x = self.bns[i](x, batch = batch)
+             x = f.dropout(x, p=self.dropout, training=self.training)
             x = self.activation(x)
-            x = f.dropout(x, p=self.dropout, training=self.training)
-            
+        
+        x = self.conv_last(x, ei)
         x = self.pool(x, batch)
         x = self.linear(x)
         return f.softmax(x, dim=1)
@@ -131,7 +136,7 @@ class GNNSage(nn.Module):
             )
         )
         self.bns = nn.ModuleList()
-        self.bns.append(nn.BatchNorm1d(self.hidden_channels))
+        self.bns.append(pyg_nn.LayerNorm(self.hidden_channels))
 
         for _ in range(num_layers - 1):
             self.convs.append(
@@ -140,7 +145,12 @@ class GNNSage(nn.Module):
                     self.hidden_channels,
                 )
             )
-            self.bns.append(nn.BatchNorm1d(self.hidden_channels))
+            self.bns.append(pyg_nn.LayerNorm(self.hidden_channels))
+
+        self.conv_last = pyg_nn.SAGEConv(
+            self.hidden_channels,
+            self.hidden_channels,
+        )
 
         self.dropout = dropout
         self.activation = f.relu
@@ -165,17 +175,17 @@ class GNNSage(nn.Module):
             The output of the model.
         """
         x, ei, ew, batch = data.x, data.edge_index, data.edge_attr, data.batch
-        x = f.dropout(x, p=self.dropout, training=self.training)
         for i, conv in enumerate(self.convs):
             if self.weighted:
                 x = conv(x, ei, edge_weight=ew)
             else: 
                 x = conv(x, ei)
             if self.use_bn:
-                x = self.bns[i](x)
-            x = self.activation(x)
+                x = self.bns[i](x, batch = batch)
             x = f.dropout(x, p=self.dropout, training=self.training)
-            
+            x = self.activation(x)
+
+        x = self.conv_last(x, ei)
         x = self.pool(x, batch)
         x = self.linear(x)
         return f.softmax(x, dim=1)
@@ -229,7 +239,7 @@ class GAT(nn.Module):
         )
 
         self.bns = nn.ModuleList()
-        self.bns.append(nn.BatchNorm1d(self.hidden_channels * heads))
+        self.bns.append(pyg_nn.LayerNorm(self.hidden_channels * heads))
         for _ in range(num_layers - 1):
             self.convs.append(
                 pyg_nn.GATConv(
@@ -240,8 +250,16 @@ class GAT(nn.Module):
                     concat=True,
                 )
             )
-            self.bns.append(nn.BatchNorm1d(self.hidden_channels * heads))
+            self.bns.append(pyg_nn.LayerNorm(self.hidden_channels * heads))
 
+        self.conv_last = pyg_nn.GATConv(
+            self.hidden_channels * heads,
+            self.hidden_channels,
+            dropout=dropout,
+            heads=heads,
+            concat=True,
+        )
+        
         self.dropout = dropout
         self.activation = f.elu
         self.use_bn = use_bn
@@ -270,10 +288,68 @@ class GAT(nn.Module):
             else: 
                 x = conv(x, ei)
             if self.use_bn:
-                x = self.bns[i](x)
-            x = self.activation(x)
+                x = self.bns[i](x, batch = batch)
             x = f.dropout(x, p=self.dropout, training=self.training)
+            x = self.activation(x)
 
+        x = self.conv_last(x, ei)
         x = self.pool(x, batch)
         x = self.linear(x)
+        return f.softmax(x, dim=1)
+
+class GIN(torch.nn.Module):
+    def __init__(
+        self,
+        num_layers: int,
+        nfeat: int,
+        nhid: int,
+        nclass: int,
+        dropout: float,
+        use_bn: bool = False,
+        pool: Literal["mean", "max"] = "mean"
+        ):
+        super(GIN, self).__init__()
+        self.input_channels = nfeat
+        self.output_channels = nclass
+        self.hidden_channels = nhid
+        self.num_layers = num_layers
+        
+        self.dropout = dropout
+        self.activation = f.relu
+        self.use_bn = use_bn
+        
+        self.linear0 = nn.Linear(self.input_channels, self.hidden_channels)
+        self.convs = torch.nn.ModuleList()
+        
+        for i in range(num_layers):
+            mlp = nn.Sequential(
+                nn.Linear(self.hidden_channels, self.hidden_channels), 
+                nn.BatchNorm1d(self.hidden_channels) if self.use_bn else nn.Identity(),
+                nn.ReLU(),                       
+                nn.Linear(self.hidden_channels, self.hidden_channels), 
+                nn.BatchNorm1d(self.hidden_channels) if self.use_bn else nn.Identity(),
+                nn.ReLU())
+            )
+            self.convs.append(pyg_nn.GINConv(mlp, eps=0, train_eps=False))
+        
+        self.linear1 = nn.Linear(self.hidden_channels, self.hidden_channels)
+        self.linear2 = nn.Linear(self.hidden_channels, self.output_channels)
+
+        if pool == "mean":
+          self.pool = pyg_nn.global_mean_pool
+        elif pool == "max":
+          self.pool = pyg_nn.global_max_pool
+
+    def forward(self, data):
+        x, ei, ew, batch = data.x, data.edge_index, data.edge_attr, data.batch
+        x = self.linear0(x)
+        x = f.dropout(x, p=self.dropout, training=self.training)
+        x = self.activation(x)
+        for i in range(self.num_layers):
+            x = self.convs[i](x, ei)
+        
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.pool(x)
+        x = self.linear2(x)
         return f.softmax(x, dim=1)
